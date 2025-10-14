25f2855250d9a2fec6a683b35a615e2b7bd74f60
oscarv157 <134034877+oscarv157@users.noreply.github.com>
2025-10-13 21:16:49 -0400

    Addedsome code to test whether the flask api is able to read the csv data. Also added to the streamlit page.

--- client.py
+++ client.py
@@ -2,3 +2,18 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 import requests
+
+base_url = "http://127.0.0.1:5000"
+
+st.title("Water Quality Observations")
+
+tab1, tab2= st.tabs([
+    "Tab 1",
+    "Tab 2"
+])
+
+with tab1:
+    st.write("First tab")
+    if st.button("Water Quality Observations Data Columns"):
+        response = requests.get(base_url + "/data").json()
+        st.dataframe(pd.DataFrame(response))
--- flaskAPI.py
+++ flaskAPI.py
@@ -2,4 +2,15 @@ from flask import Flask, jsonify
 import pandas as pd
 
 app = Flask(__name__)
+df = pd.read_csv("cleaned_datasets/2022-nov16.csv")
 
+@app.route('/')
+def index():
+    return "Water Quality Flask API. route:/data"
+
+@app.route("/data")
+def data():
+    return jsonify(df.head(5).to_dict(orient="records"))
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
