94f6417355b2bf8ff95c6da9f95f306b435deb28
frankpuerta <fpuerta14@gmail.com>
2025-10-14 11:30:44 -0400

    Fix streamlit structure

--- client.py
+++ client.py
@@ -48,17 +48,3 @@ with scatterPlot:
 with maps:
     st.write("Maps")
     
-base_url = "http://127.0.0.1:5000"
-
-st.title("Water Quality Observations")
-
-tab1, tab2= st.tabs([
-    "Tab 1",
-    "Tab 2"
-])
-
-with tab1:
-    st.write("First tab")
-    if st.button("Water Quality Observations Data Columns"):
-        response = requests.get(base_url + "/data").json()
-        st.dataframe(pd.DataFrame(response))
